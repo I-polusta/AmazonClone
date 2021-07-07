734670894d7647fd1dcd812af6d029d983b80690
I-polusta <76556107+I-polusta@users.noreply.github.com>
2021-07-07 09:08:09 +0530

    Delete axios.js

--- axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  baseURL: "http://localhost:5001/clone-925d9/us-central1/api",
-  //the API (cloud func) URL
-});
-
-export default instance;
